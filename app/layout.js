392ef9a601aba16e962aa3962b925a255bda8d76
hanzalasarwar <hanzala.sarwar.dev@gmail.com>
2026-05-10 12:55:28 +0500

    improving SEO

--- app/layout.js
+++ app/layout.js
@@ -19,14 +19,16 @@ export const metadata = {
     "URL shortener",
     "link shortener",
     "short links",
-    "Next.js project",
+    "Next.js",
     "BitLinks",
   ],
 
   authors: [{ name: "Hanzala" }],
   creator: "Hanzala",
 
-  metadataBase: new URL("https://url-shortener-henna-three.vercel.app"),
+  metadataBase: new URL(
+    "https://url-shortener-henna-three.vercel.app"
+  ),
 
   openGraph: {
     title: "BitLinks - Modern URL Shortener",
@@ -36,17 +38,40 @@ export const metadata = {
     siteName: "BitLinks",
     locale: "en_US",
     type: "website",
+
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "BitLinks Preview Image",
+      },
+    ],
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "BitLinks",
-    description: "Modern URL shortening platform",
+    title: "BitLinks - Modern URL Shortener",
+    description:
+      "Create and manage short links instantly with BitLinks.",
+
+    images: ["/twitter-image.png"],
   },
 
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
+  icons: {
+    icon: "/favicon.ico",
   },
 };
 
